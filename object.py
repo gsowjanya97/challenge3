f2e6e04ce402dcba6947cfd5798847a2320c3d35
gsowjanya97 <sowjanya.gade@outlook.com>
2023-10-02 22:25:43 +0530

    Updated comments

--- object.py
+++ object.py
@@ -3,13 +3,13 @@ object = {'a':{'b':{'c':'d'}}}
 def getvalue(value,s):
     while(value != None):
         try:
-            key=list(value.keys())[0]    #Get Key of dictionay
+            key=list(value.keys())[0]    #Get the first key of dictionary
             if (s==key):
                 print(value.get(key))
-                break                   #If Key matches, provides output of value
-            value=value.get(key)
+                break                   #If the key matches, provide output of value and exit the loop
+            value=value.get(key)        #Else start again with the nested dictionary
         except:
-            print("Key not found")
+            print("Key not found")      #When reached at the end of dictionary, avoid exceptions
             break
         
         
